937e9787d307b4a300930aca633ecf1a7a5f10b9
yukihyogo <secondaryshrike@gmail.com>
2015-09-12 01:45:30 +0000

    add initial setups

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,14 @@ module Microposts
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # added for microposts on Sep,09,15(Tue)
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+    
   end
 end
