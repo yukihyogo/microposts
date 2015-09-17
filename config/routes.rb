4aef6bce4151dbc0d3a4bc7e61f536a4161341b4
yukihyogo <secondaryshrike@gmail.com>
2015-09-17 06:04:38 +0000

    add sign sign up and user detail

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+#  get 'users/new'
+
+#  root to: 'static_pages#home'
+
   root to: 'static_pages#home'
+  get 'signup',  to: 'users#new'
+
+  resources :users
 end
