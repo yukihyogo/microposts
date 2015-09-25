983370fa9d936609b977c0b8ffe9721f75c76df6
yukihyogo <secondaryshrike@gmail.com>
2015-09-25 16:01:21 +0000

    delete dup def from migrate/_create_users

--- db/migrate/20150913130245_create_users.rb
+++ db/migrate/20150913130245_create_users.rb
@@ -5,9 +5,6 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email
       t.string :password_digest
 
-      t.string :place
-      t.string :profile
-
       t.timestamps null: false
       
       t.index :email, unique: true
